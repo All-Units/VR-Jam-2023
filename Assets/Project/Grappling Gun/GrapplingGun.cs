03e5b1d58c42ae7de29cbabde934093b45c8fa3f
Zach <zdayscott@gmail.com>
2023-06-23 21:14:47 -0700

    Grapple Gun V1

---
+++ Assets/Project/Grappling Gun/GrapplingGun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GrapplingGun : MonoBehaviour {
+    
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappleable;
+    public Transform gunTip;
+    private float maxDistance = 100f;
+    private bool isGrappling;
+
+    /// <summary>
+    /// Call whenever we want to start a grapple
+    /// </summary>
+    public void StartGrapple() {
+        RaycastHit hit;
+        if (Physics.Raycast(gunTip.position, gunTip.forward, out hit, maxDistance, whatIsGrappleable)) {
+            grapplePoint = hit.point;
+            isGrappling = true;
+        }
+    }
+
+
+    /// <summary>
+    /// Call whenever we want to stop a grapple
+    /// </summary>
+    public void StopGrapple()
+    {
+        isGrappling = false;
+    }
+
+
+
+    public bool IsGrappling() {
+        return isGrappling;
+    }
+
+    public Vector3 GetGrapplePoint() {
+        return grapplePoint;
+    }
+}
---
+++ Assets/Project/Grappling Gun/GrapplingRope.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class GrapplingRope : MonoBehaviour {
+    private Spring spring;
+    private LineRenderer lr;
+    private Vector3 currentGrapplePosition;
+    public GrapplingGun grapplingGun;
+    public int quality;
+    public float damper;
+    public float strength;
+    public float velocity;
+    public float waveCount;
+    public float waveHeight;
+    public AnimationCurve affectCurve;
+    [SerializeField] private float maxLerpTime = 12f;
+    private float currentLerpTime = 0;
+    
+
+    void Awake() {
+        lr = GetComponent<LineRenderer>();
+        spring = new Spring();
+        spring.SetTarget(0);
+    }
+    
+    //Called after Update
+    void LateUpdate() {
+        DrawRope();
+    }
+
+    void DrawRope() 
+    {
+        var grapplePoint = grapplingGun.GetGrapplePoint();
+        var gunTipPosition = grapplingGun.gunTip.position;
+        var up = Quaternion.LookRotation((grapplePoint - gunTipPosition).normalized) * Vector3.up;
+
+
+        
+        //If not grappling, don't draw rope
+        if (!grapplingGun.IsGrappling())
+        {
+            currentLerpTime -= Time.deltaTime * 2;
+            currentLerpTime = Mathf.Clamp(currentLerpTime, 0, maxLerpTime);
+            
+            if(currentLerpTime <= 0)
+            {
+                currentGrapplePosition = grapplingGun.gunTip.position;
+                spring.Reset();
+                if (lr.positionCount > 0)
+                    lr.positionCount = 0;
+                return;
+            }
+        }
+        else
+        {
+            currentLerpTime += Time.deltaTime;
+            currentLerpTime = Mathf.Clamp(currentLerpTime, 0, maxLerpTime);
+        }
+
+        if (lr.positionCount == 0) {
+            spring.SetVelocity(velocity);
+            lr.positionCount = quality + 1;
+        }
+        
+        spring.SetDamper(damper);
+        spring.SetStrength(strength);
+        spring.Update(Time.deltaTime);
+        
+        currentGrapplePosition = Vector3.Lerp(gunTipPosition, grapplePoint, currentLerpTime / maxLerpTime);
+        DrawPoints(up, gunTipPosition);
+    }
+
+    private void DrawPoints(Vector3 up, Vector3 gunTipPosition)
+    {
+        for (var i = 0; i < quality + 1; i++)
+        {
+            var delta = i / (float)quality;
+            var offset = up * waveHeight * Mathf.Sin(delta * waveCount * Mathf.PI) * spring.Value *
+                         affectCurve.Evaluate(delta);
+
+            lr.SetPosition(i, Vector3.Lerp(gunTipPosition, currentGrapplePosition, delta) + offset);
+        }
+    }
+}
