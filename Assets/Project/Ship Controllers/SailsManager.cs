fade06707b1d5aadd7f436357ba34c89706b5437
Zach <zdayscott@gmail.com>
2023-06-24 11:13:10 -0700

    Update SailsManager.cs
    - Made it a singleton

--- Assets/Project/Ship Controllers/SailsManager.cs
+++ Assets/Project/Ship Controllers/SailsManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -10,6 +12,14 @@ public class SailsManager : MonoBehaviour
     [Range(0,1)]
     public float currentValue;
 
+    public static SailsManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [Editor]
     private void Update()
     {
         foreach (SailController sailController in sailControllers)
@@ -17,5 +27,16 @@ public class SailsManager : MonoBehaviour
             sailController.UpdateSail(currentValue);
         }
     }
-    
+
+    /// <summary>
+    /// Controls all sails
+    /// </summary>
+    /// <param name="val">Value will be clamped to between 0-1</param>
+    public void UpdateSailValue(float val)
+    {
+        foreach (var sailController in sailControllers)
+        {
+            sailController.UpdateSail(val);
+        }
+    }
 }
